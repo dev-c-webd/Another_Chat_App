0befded86798ca05726b49aa01b3c3ff314df240
kalash <kalash21504@gmail.com>
2025-10-30 19:52:23 +0530

    added useGetConversations hook and zustand for global state

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,17 +1,6 @@
-// import { useAuthContext } from "../../context/AuthContext";
-// import { extractTime } from "../../utils/extractTime";
-// import useConversation from "../../zustand/useConversation";
+
 
 const Message = ({ message }) => {
-	// const { authUser } = useAuthContext();
-	// const { selectedConversation } = useConversation();
-	// const fromMe = message.senderId === authUser._id;
-	// const formattedTime = extractTime(message.createdAt);
-	// const chatClassName = fromMe ? "chat-end" : "chat-start";
-	// const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
-	// const bubbleBgColor = fromMe ? "bg-blue-500" : "";
-
-	// const shakeClass = message.shouldShake ? "shake" : "";
 
 	return (
 		<div className={`chat`}>
@@ -20,7 +9,7 @@ const Message = ({ message }) => {
 					<img alt='Tailwind CSS chat bubble component' />
 				</div>
 			</div>
-			<div className={`chat-bubble text-white pb-2`}>eljbdckj</div>
+			<div className={`chat-bubble text-white pb-2`}>any message</div>
 			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>12:00</div>
 		</div>
 	);
